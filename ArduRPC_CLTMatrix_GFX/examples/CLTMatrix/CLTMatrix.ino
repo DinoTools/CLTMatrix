3b8013ee4bdd75b045f6beebbfd9ff6b53230f29
PhiBo <phibo@dinotools.org>
2015-03-15 17:34:37 +0100

    rpc - Update to build firmware for ArduRPC 0.4

--- ArduRPC_CLTMatrix_GFX/examples/CLTMatrix/CLTMatrix.ino
+++ ArduRPC_CLTMatrix_GFX/examples/CLTMatrix/CLTMatrix.ino
@@ -8,65 +8,59 @@
  * - ArduRPC_Adafruit_DHT - https://github.com/DinoTools/ArduRPC-handlers
  */
 
-// Load libraries for the DHT11 sensor
+/* Load libraries for the DHT11 sensor */
 #include <DHT.h>
 
+/* Load libraries for the CLTMatrix */
+#include <Adafruit_GFX.h>
+#include <CLTMatrix_GFX.h>
+
+
+/* Load libraries and wrappers for ArduRPC */
+#include <ArduRPC.h>
+#include <ArduRPC_CLTMatrix_GFX.h>
+#include <ArduRPC_Adafruit_DHT.h>
+
+
+/* Initialize AruRPC interface */
+
+/* the serial port, it's also possible to use software serial */
+#define RPC_SERIAL_PORT Serial
+#define RPC_SERIAL_BAUD 19200
+
+#define RPC_NUM_HANDLERS 3
+#define RPC_NUM_FUNCTIONS 0
+
+ArduRPC rpc = ArduRPC(RPC_NUM_HANDLERS, RPC_NUM_FUNCTIONS);
+ArduRPC_Serial rpc_serial = ArduRPC_Serial(RPC_SERIAL_PORT, rpc);
+
+
+/* Configure the Sensor */
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
-// Load libraries for the CLTMatrix
-#include <Adafruit_GFX.h>
-#include <CLTMatrix_GFX.h>
+ArduRPC_Adafruit_DHT Adafruit_DHT_Wrapper(rpc, "sensor", dht, DHTTYPE);
 
+
+/* Configurate the matrix */
 CLTMatrix Matrix;
 
-// Load libraries and wrappers for ArduRPC
-#include <ArduRPC.h>
-#include <ArduRPC_CLTMatrix_GFX.h>
-#include <ArduRPC_Adafruit_DHT.h>
-
-#define RPC_NUM_HANDLERS 3
-#define RPC_NUM_FUNCTIONS 0
-
-// the serial port, it's also possible to use software serial
-#define RPC_SERIAL_PORT Serial
-#define RPC_SERIAL_BAUD 19200
-
-ArduRPC rpc = ArduRPC(RPC_NUM_HANDLERS, RPC_NUM_FUNCTIONS);
-ArduRPCSerial rpc_serial = ArduRPCSerial(RPC_SERIAL_PORT, rpc);
+ArduRPC_CLTMatrix_GFX CLTMatrix_GFX_Wrapper(rpc, "matrix", Matrix);
 
 void setup() {
-  uint8_t handler_id;
-
-  // Initialize the serial port
+  /* Initialize the serial port */
   RPC_SERIAL_PORT.begin(RPC_SERIAL_BAUD);
-
-  /* Matrix */
   
-  // Initialize and active the Matrix
-  Matrix.init();
-  Matrix.begin();
-
-  // Register the ArduRPC wrapper
-  handler_id = rpc.connectHandler(get_ArduRPC_CLTMatrix_GFX_Wrapper(Matrix));
-
-  // Set handler name for the Matrix
-  rpc.setHandlerName(handler_id, "matrix");
-  
-  /* Sensor */
-  
-  // Activate the sensor
+  /* Activate the sensor */
   dht.begin();
   
-  // Register the ArduRPC wrapper
-  handler_id = rpc.connectHandler(get_ArduRPC_Adafruit_DHT_Wrapper(dht, DHTTYPE));
-
-  // Set handler name for the Sensor
-  rpc.setHandlerName(handler_id, "sensor");
+  /* Initialize and activate the Matrix */
+  Matrix.init();
+  Matrix.begin();
 }
 
 void loop() {
-  rpc_serial.loop();
+  rpc_serial.readData();
 }
